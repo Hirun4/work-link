92e658103c39c566f70e589bc833facbb1403840
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2025-01-24 07:26:33 +0530

    cloudinary

--- Backend/utils/cloudinary.js
+++ Backend/utils/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: "your-cloud-name",
-  api_key: "your-api-key",
-  api_secret: "your-api-secret",
+  cloud_name: "dvcvgmi80",
+  api_key: "957969932752553",
+  api_secret: "iNj9CRD_TuzmkkvxaE-9XC89ssY",
 });
 
 module.exports = cloudinary;
